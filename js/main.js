1e68974d1166ab6af0044c6ca1edd0db3cf2da2c
emilycopp <emily.copp@bell.net>
2016-03-12 14:56:38 -0500

    organized veggies

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ var $newUl = $('<ul>');
 var $newHeadingAbove = $('<h1>');
 var $newUl2 = $('<ul>');
 
+$newHeadingBelow.html('Below ground veggies');
+$('body').append($newHeadingBelow);
+$('body').append($newUl);
+
+$newHeadingAbove.html('Above ground veggies');
+$('body').append($newHeadingAbove);
+$('body').append($newUl2);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $newUl.append($(this));
+  } else {
+    $newUl2.append($(this));
+  }
+});
